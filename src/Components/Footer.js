d60683420ca471dfccb29847a320d66b2f461619
Blake Trapnell <Blake.Trapnell103@gmail.com>
2019-07-22 15:55:56 -0600

    final update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,70 +1,101 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import "./Header.css"
 import Logo from "./Logo"
 import axios from "axios"
 
 export default class Footer extends Component {
-constructor() {
-    super() 
-    this.state = {
-        sprite: "",
-        name: "",
-        clicked: false,
-        pokedex: 0,
-        guessed: "",
+    constructor() {
+        super()
+        this.state = {
+            sprite: "",
+            name: "",
+            clicked: false,
+            pokedex: 0,
+            guessed: "",
+            score: 0,
+            correctGuess: [],
+            incorrectGuess: [],
+        }
     }
-}
-getSprite() {
-    let id = Math.ceil(Math.random()*802)
-    console.log(id)
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then(res=> ( 
+    getSprite() {
+        let id = Math.ceil(Math.random() * 802)
+        console.log(id)
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            .then(res => (
+                this.setState({
+                    pokedex: id,
+                    sprite: res.data.sprites.front_shiny,
+                    name: res.data.name,
+                    clicked: true
+                })))
+    }
+    changeGuessed(e) {
         this.setState({
-        pokedex: id,
-        sprite: res.data.sprites.front_shiny,
-        name: res.data.name,
-        clicked: true})))
-}
-changeGuessed(e) {
-    this.setState({
-        guessed: e
-    })
-}
-
-guessThatPokemon() {
-    console.log("is this working")
-    if (this.state.guessed.toLowerCase() === this.state.name.toLowerCase()) {
-        alert(`Correct! Pokedex Number: ${this.state.pokedex}`)
+            guessed: e
+        })
     }
-    else {
-        alert(`not quite! the correct name is ${this.state.name}. Pokedex Number: ${this.state.pokedex}`)
-    }
-}
 
-render() {
-    const {resetHH} = this.props
-    return(
-           <footer>
-              {this.state.clicked ? (
-                  <div className="bottom">
-                  <img className="pokemon" src={this.state.sprite} alt=""/>
-                  <input type="text" placeholder="Pokemon name" onChange={(e)=> this.changeGuessed(e.target.value)}/>
-                  <button onClick={()=>this.guessThatPokemon()}>guess that pokemon!</button>
-                  <button onClick={()=>this.getSprite()}>New Pokemon!</button>
-                  </div>
-                  
-              ) : (
-                  <div className= "bottom">
-                <Logo/>
-              <button onClick={()=>this.getSprite()}>Click Me!</button>
-               <h1>Thank you Sam, Matt, and Jonathan!</h1>
-               <button onClick={()=>resetHH()}>Reset</button>
-               <Logo/>
-                  </div>
-              ) 
-              }
-           </footer>
-    )
-}
+    guessThatPokemon() {
+        if (this.state.guessed.toLowerCase() === this.state.name.toLowerCase()) {
+            const newScore = this.state.score + 10
+            const temp = this.state.correctGuess
+            temp.push({ name: this.state.name, sprite: this.state.sprite })
+            this.setState({
+                score: newScore,
+                correctGuess: temp
+            })
+            alert(`Right! It's ${this.state.name}! Pokedex Number: ${this.state.pokedex}`)
+            this.getSprite()
+
+        }
+        else {
+            alert(`not quite! the correct name is ${this.state.name}. Pokedex Number: ${this.state.pokedex}`)
+            const newScore = this.state.score - 3
+            const temp = this.state.incorrectGuess
+            temp.push({ name: this.state.name, sprite: this.state.sprite })
+            this.setState({
+                score: newScore,
+                incorrectGuess: temp
+            })
+            this.getSprite()
+        }
+    }
+
+    render() {
+        const { resetHH } = this.props
+        const { incorrectGuess, correctGuess, sprite, score, clicked } = this.state
+        return (
+            <footer>
+                {clicked ? (
+                    <div className="bottom">
+                        <img className="pokemon" src={sprite} alt="" />
+                        <input type="text" placeholder="Pokemon name" onChange={(e) => this.changeGuessed(e.target.value)} />
+                        <button onClick={() => this.guessThatPokemon()}>Who's that pokemon?</button>
+                        <h3>Your score {score}</h3>
+                        {correctGuess.map((el) => (
+                            <div>
+                                <h3>Correct: <p>{el.name}</p> </h3>
+                                <img src={el.sprite} alt=""/>
+                            </div>))}
+                        {incorrectGuess.map((el) => (
+                            <div>
+                                <h3>incorrect: <p>{el.name}</p></h3>
+                                <img src={el.sprite} alt=""/>
+                            </div>))}
+                    </div>
+
+                ) : (
+                        <div className="bottom">
+                            <Logo />
+                            <button onClick={() => this.getSprite()}>Click Me!</button>
+                            <h1>Thank you Sam, Matt, and Jonathan!</h1>
+                            <button onClick={() => resetHH()}>Reset</button>
+                            <Logo />
+                        </div>
+                    )
+                }
+            </footer>
+        )
+    }
 
 }
